17578455f0572387f3e59417321b7cf282e7f1d9
DIALLO <diallo@MacBook-Pro-de-DIALLO.local>
2026-03-04 10:53:17 +0100

    Jwt Security : Token , Filter , SecurityFilter

---
+++ src/main/java/com/tpspringboot/apirestclientcommande/Client/modeleCL/User.java
@@ -0,0 +1,17 @@
+package com.tpspringboot.apirestclientcommande.Client.modeleCL;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String username ;
+    private String password ;
+    private String role ;
+}
